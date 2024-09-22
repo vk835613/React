597c8e5f78b29ccb5f72e11680abd9783ba1d194
vk835613 <vk835613@gmail.com>
2024-09-22 14:47:06 +0530

    Mega Blog Project

---
+++ 12MegaBlog/src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function Logo({width = '100px'}) {
+  return (
+    <img src="logo.png" className='h-14'/>
+  )
+}
+
+export default Logo
---
+++ 12MegaBlog/src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import React, {useEffect, useState} from 'react'
+import appwriteService from "../appwrite/config";
+import {Container, PostCard} from '../components'
+
+function Home() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        appwriteService.getPosts().then((posts) => {
+            if (posts) {
+                setPosts(posts.documents)
+            }
+        })
+    }, [])
+  
+    if (posts.length === 0) {
+        return (
+            <div className="w-full h-screen py-8 text-center bg-cover bg-[url('https://images.unsplash.com/photo-1499728603263-13726abce5fd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
+                <Container>
+                    <div className="flex flex-wrap">
+                        <div className="p-2 w-full ">
+                            <h1 className="text-2xl font-bold hover:text-gray-500 ">
+                                Login to read posts
+                            </h1>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        )
+    }
+    return (
+        <div className='w-full py-8'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {posts.map((post) => (
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard {...post} />
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default Home
